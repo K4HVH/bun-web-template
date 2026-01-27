d75289c03020bff7fdfded650dbd65f14e89fb70
K4HVH <bronercast@gmail.com>
2026-01-27 14:05:30 +1000

    Cleanup of file structure

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,13 @@ import devtools from 'solid-devtools/vite';
 
 export default defineConfig({
   plugins: [devtools(), solidPlugin()],
+  root: 'src',
   server: {
     port: 3000,
   },
   build: {
     target: 'esnext',
+    outDir: '../dist',
+    emptyOutDir: true,
   },
 });
